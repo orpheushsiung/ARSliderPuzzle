7614c16c0da5bbb0a83c2a3bb89c245d798e90e1
orpheushsiung <53945679+orpheushsiung@users.noreply.github.com>
2021-06-27 15:38:18 +0800

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,10 +1,10 @@
 {
-    // New Game-Level Variables
-    const image = new Image(),
-    	takePhotoButton = document.querySelector('.takePhoto');
-    let constraints, imageCapture, mediaStream, video;
+	const image = new Image(),
+		takePhotoButton = document.querySelector('.takePhoto');
 
-    // Puzzle Vars
+	let constraints, imageCapture, mediaStream, video;
+
+	//Puzzle Vars
 	const markers = document.querySelectorAll(`a-marker`),
 		numCol = 3, numRow = 3,
 		puzzlePieces = numCol * numRow,
@@ -55,48 +55,131 @@
 
 	};
 
-    // Take the picture
-    const getPicture = () => {
+	//Take the picture 
+	const getPicture = () => { 
 		//shuffle(puzzle);
-        imageCapture.takePhoto()
-            .then((img) => {
-                image.src = URL.createObjectURL(img);
-                image.addEventListener('load', () => createImagePieces(image));
-                setInterval(() => checkDistance(), 1000);
-                console.log(puzzle);
-            })
-            .catch((error) => {console.log('takePhoto() error', error)});
-    };
+		imageCapture.takePhoto()
+			.then((img) => {
+				image.src = URL.createObjectURL(img);
+				image.addEventListener('load', () => createImagePieces(image));
+				setInterval(() => checkDistance(), 1000);
+				console.log(puzzle);
+			})
+			.catch((error) => {console.log('takePhoto() error', error)});
 
-    const createImagePieces = image => {
-        const canvas = document.createElement(`canvas`);
-        const ctx = canvas.getContext('2d');
-        const pieceWidth = image.width / numCol;
-        const pieceHeight = image.height /numRow;
+	};
 
-        for (let x = 0; x < numCol; x++) {
-            for (let y = 0; y < numRow; y++) {
-                ctx.drawImage(image, x * pieceWidth, y * pieceHeight, pieceWidth, pieceHeight, 0, 0, canvas.width, canvas.height);
-                imgPieces[8 - pieces] = canvas.toDataURL("image/png").replace("image/png","image/octet-stream");
-                console.log(imgPieces);
-                pieces = pieces - 3;
-                if (pieces < 0) {
-                    pieces = (puzzlePieces - 1) + pieces;
-                }
-            }
-        };
 
-        markers.forEach((marker, index) => {
-            const aImg = document.createElement(`a-image`);
-            aImg.setAttribute(`rotation`, `-90 0 0`);
-            aImg.setAttribute(`position`, `0 0 0`);
-            aImg.setAttribute(`src`, imgPieces[puzzle[index]]);
-            marker.appendChild(aImg);
-        });
+	const createImagePieces = image => {
 
-    };
-    const checkDistance = () => {};
+		const canvas = document.createElement(`canvas`);
+		const ctx = canvas.getContext('2d');
+		const pieceWidth = image.width / numCol;
+		const pieceHeigth = image.height /numRow;
 
-    window.addEventListener(`load`, () => setTimeout(() => init(), 1000));
+
+		for (let x = 0; x < numCol; ++x) {
+			for (let y = 0; y < numRow; ++y) {
+				ctx.drawImage(image, x * pieceWidth, y * pieceHeigth, pieceWidth, pieceHeigth, 0, 0, canvas.width, canvas.height);
+				imgPieces[8 - pieces] = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+				console.log(imgPieces);
+				pieces = pieces - 3;
+				if (pieces < 0) {
+					pieces = (puzzlePieces - 1) + pieces;
+				}
+
+			}
+		};
+
+		markers.forEach((marker, i) => {
+			const aImg = document.createElement(`a-image`);
+
+			aImg.setAttribute(`rotation`, `-90 0 0`);
+			aImg.setAttribute(`position`, `0 0 0`);
+			aImg.setAttribute(`src`, imgPieces[puzzle[i]]);
+
+			marker.appendChild(aImg);
+		})
+
+	}
+
+
+	const checkDistance = () => {
+
+		for(let i = 0; i < markers.length; i++){
+			positionMarkers[i] = markers[i].object3D;
+		}
+
+		if(positionMarkers[puzzle[0]].position.x - positionMarkers[puzzle[8]].position.x !== 0){
+			for(let i = 0; i < numRow; ++i){
+				if(Math.abs(positionMarkers[puzzle[0 + (3 * i)]].position.x - positionMarkers[puzzle[1+ (3 * i)]].position.x) < tolerance && 
+					Math.abs(positionMarkers[puzzle[1 + (3 * i)]].position.x - positionMarkers[puzzle[2 + (3 * i)]].position.x) < tolerance &&
+					Math.abs(positionMarkers[puzzle[0 + (3 * i)]].rotation.x - positionMarkers[puzzle[1+ (3 * i)]].rotation.x) < tolerance &&
+					Math.abs(positionMarkers[puzzle[1 + (3 * i)]].rotation.x - positionMarkers[puzzle[2 + (3 * i)]].rotation.x) < tolerance) {
+
+					check[i] = true;
+				} else {
+					check[i] = false;
+				}  	 
+			}
+
+			for(let i =0; i < numCol; ++i) {
+				if(Math.abs(positionMarkers[puzzle[i]].position.y - positionMarkers[puzzle[3 + i]].position.y ) < tolerance &&
+					Math.abs(positionMarkers[puzzle[3 + i]].position.y - positionMarkers[puzzle[6 + i]].position.y ) < tolerance &&
+					 Math.abs(positionMarkers[puzzle[i]].rotation.y - positionMarkers[puzzle[3 + i]].rotation.y ) < tolerance &&
+					 Math.abs(positionMarkers[puzzle[3 + i]].rotation.y - positionMarkers[puzzle[6 + i]].rotation.y ) < tolerance) {
+					check[3+i] = true;
+				} else {
+					check[3+i] = false;
+				}
+			}
+
+			if(check.every(puzzleCheck)) {
+				console.log('Solved!!!');
+				const solved = document.querySelector(`.solved`);
+				solved.style.display = "flex";
+			}
+
+
+
+
+
+		}
+
+		
+
+	}
+
+	const puzzleCheck = check => check === true;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	const shuffle = randomArray => {
+		for(let i = randomArray.length - 1; i > 0; i--) {
+			//random from 0 to i
+			const j = Math.floor(Math.random() * (i + 1));
+			// let t = randomArray[i];
+			// randomArray[i] = randomArray[j];
+			// randomArray[j] = t;
+			[randomArray[i], randomArray[j]] = [randomArray[j], randomArray[i]];
+		}
+		return randomArray;
+	}
+
+
+
+	window.addEventListener(`load`, () => setTimeout(() => init(), 1000));
 
 }
